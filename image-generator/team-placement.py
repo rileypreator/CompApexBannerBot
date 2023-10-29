cd54aaad31d7410965774cf72d3b7102d03d08d3
Riley Preator <60445429+rileypreator@users.noreply.github.com>
2023-10-29 09:55:30 -0600

    Create object used to house team information

---
+++ image-generator/team-placement.py
@@ -0,0 +1,6 @@
+class team_place:
+    def __init__(self, team_name, team_abrev, rank, team_points):
+        self.team_name = team_name
+        self.team_abrev = team_abrev
+        self.rank = rank
+        self.team_points = team_points
