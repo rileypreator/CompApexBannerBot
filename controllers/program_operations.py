3bbc836e95700cbf0d6a7aec9b8fa204b68e5255
Riley Preator <60445429+rileypreator@users.noreply.github.com>
2023-11-05 10:52:30 -0700

    Added Git ignore to file

---
+++ controllers/program_operations.py
@@ -0,0 +1,9 @@
+from image_classes.define_image import BannerImage
+
+def startup():
+    # create a BannerImage object
+    banner_image = BannerImage()
+    
+    # do any other startup tasks here
+    
+    print("Banner sizes:", banner_image.width, " x ", banner_image.height)
--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 This is the startup file for the application. 
 Everything gets started from here.
 """
-
+from controllers.program_operations import startup
 
 # call the start_app function to start the application
 if __name__ == "__main__":
-    start_app()
+    startup()
