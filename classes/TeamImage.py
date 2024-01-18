d8282a376ccb66fa28932cc784cdbb95b0620ae3
Riley Preator <60445429+rileypreator@users.noreply.github.com>
2024-01-18 16:07:26 -0700

    Add image overlay function

--- classes/TeamImage.py
+++ classes/TeamImage.py
@@ -39,38 +39,45 @@ class TeamImage:
         self.improvement = improvement
         self.rank = rank
 
+        # Find the team logo path or default to the normal Apex logo
+        self.team_logo_image_path = "images/team_images/" + self.teamAbrv + "_logo.png"
+        if not os.path.exists(self.team_logo_image_path):
+            self.team_logo_image_path = "images/team_images/default_team_logo.png"
 
-        self.image_path = "images/team_images/" + self.teamAbrv + "_logo.png"
-
-        if not os.path.exists(self.image_path):
-            self.image_path = "images/team_images/default_team_logo.png"
+        self.team_logo_image = cv2.imread(self.team_logo_image_path, cv2.IMREAD_UNCHANGED)
 
         # Create the team image
         self.team_placement_image = self.generate_image()
 
     def get_image_dimensions(self):
-        if os.path.exists(self.image_path):
-            with Image.open(self.image_path) as img:
+        if os.path.exists(self.team_logo_image_path):
+            with Image.open(self.team_logo_image_path) as img:
                 return img.size
 
     def generate_image(self):
-        # TODO: Generate the image
         image_size = self.get_image_dimensions()
 
         if image_size == -1:
-            raise FileNotFoundError(f"Image not found: {self.image_path}")
+            raise FileNotFoundError(f"Image not found: {self.team_logo_image_path}")
 
         # Create brand new image with grey background
         color = (215, 224, 218)
         blank_image = np.full((self.height, self.width, 3), color, np.uint8)
 
+        # Add alpha layer to image
         alpha_image = self.add_alpha_channel(blank_image)
 
-        rounded_image =  self.add_rounded_border(alpha_image, 3, (255, 255, 255))
+        # Add border to overall image
+        rounded_image =  self.add_rounded_border(alpha_image, 5, (255, 255, 255))
 
-        cv2.imshow("Rounded Image", rounded_image)
-        cv2.waitKey()
-        return Image.open(self.image_path)
+        # Add team logo to image
+        team_image = self.add_team_image(rounded_image)
+
+
+        # cv2.imshow("Rounded Image", team_image)
+        # cv2.waitKey()
+        cv2.imwrite("images/team_placement_images/" + self.teamAbrv + "_placement.png", team_image)
+        return Image.open(self.team_logo_image_path)
 
 
     # add a border around the initial image to make it so that the border is added with the color provided
@@ -110,6 +117,25 @@ class TeamImage:
     def add_alpha_channel(self, image):
         b_channel, g_channel, r_channel = cv2.split(image)
         alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 50 #creating a dummy alpha channel image.
+
+        alpha_channel = alpha_channel.astype(np.uint8)
         alpha_image = cv2.merge((b_channel, g_channel, r_channel, alpha_channel))
 
         return alpha_image
+
+    # add team logo to the image
+    def add_team_image(self, image):
+
+        if (self.rank == 1):
+            x_offset = 95
+            y_offset = 15
+        elif (self.rank == 2 or 3):
+            x_offset = 95
+            y_offset = 15
+        else:
+            x_offset = 95
+            y_offset = 15
+
+        image[y_offset: y_offset + self.team_logo_image.shape[0], x_offset: x_offset + self.team_logo_image.shape[1]] = self.team_logo_image
+
+        return image
