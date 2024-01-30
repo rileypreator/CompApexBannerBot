82a4bdbc3a26cbdee4c21ea66f4b2a16f3915d88
Riley Preator <60445429+rileypreator@users.noreply.github.com>
2024-01-29 17:24:59 -0700

    Added command-line argument support for subreddit stats

--- controllers/program_operations.py
+++ controllers/program_operations.py
@@ -1,17 +1,26 @@
 """
 Created by: Riley Preator
 Created on: 11/05/2023
-Last modified on: 12/14/2023
+Last modified on: 1/29/2024
 """
 
 from classes.BannerImage import BannerImage
 from imports.imports import cv2
 from imports.imports import json
+from imports.imports import sys
 from controllers.ranking_operations import create_new_rankings
+from controllers.api_operations import api_startup
 
 def run_main_operation():
     startup()
 
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == "stats":
+            api_result = api_startup(True)
+        else:
+            api_result = api_startup(False)
+    else:
+        api_result = api_startup(False)
     previous_rankings = get_previous_rankings()
 
     create_new_rankings(previous_rankings)
--- imports/imports.py
+++ imports/imports.py
@@ -1,7 +1,7 @@
 """
 Created by: Riley Preator
 Created on: 10/29/2023
-Last modified on: 1/18/2024
+Last modified on: 1/29/2024
 """
 
 
@@ -9,17 +9,14 @@ Last modified on: 1/18/2024
 This module contains the necessary imports for the CompApexBannerBot project.
 """
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 import cv2
 import os
-import numpy as np
 from tkinter import *
-import importlib
-import datetime
 import cv2
 import praw
-import requests
 import json
 import random
+import re
 from PIL import Image, ImageDraw, ImageFont
+from collections import Counter
+import sys
